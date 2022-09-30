7f39e702cf3159831072d68b7201db6522806f9a
Popsana Barida <popsyjunior@gmail.com>
2022-09-30 18:54:41 +0100

    add go-module-path argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
-ag "$1" @asyncapi/markdown-template -0 "./$2"
-echo "Generating markdown template from $1 to directory - $2"
-echo "::set-output name=status::Completed"
+# Generate the yaml file..
+echo "docs/asyncapi.yml => $1"
+echo "app/docs => $2"
+echo "app/docs => $3"
+
+# Generate the markdown file
+# ag "$1" @asyncapi/markdown-template -0 "./$2"
+# echo "Generating markdown template from $1 to directory - $2"
+# echo "::set-output name=status::Completed"
