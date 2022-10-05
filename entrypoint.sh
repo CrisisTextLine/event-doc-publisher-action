0a0c06544f3b98c358f336fc7df16af803ccf276
Popsana Barida <popsyjunior@gmail.com>
2022-10-05 10:07:04 +0100

    update action; remove inputs.go-module-path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -l
 
-# Generate the yaml file..
-echo "docs/asyncapi.yml => $1"
-echo "app/docs => $2"
-echo "app/docs => $3"
-
 # Generate the markdown file
-ls -alh
-ag "$1" @asyncapi/markdown-template -o "./$2"
 echo "Generating markdown template from $1 to directory - $2"
+ag "$1" @asyncapi/markdown-template -o "./$2"
 echo "::set-output name=status::Completed"
