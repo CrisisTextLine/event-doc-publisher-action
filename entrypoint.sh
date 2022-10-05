1f9cad239f1dfe71dd6456aa2dcd0045565828a6
Popsana Barida <popsyjunior@gmail.com>
2022-10-05 05:01:02 +0100

    show directory listing; fix output flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "app/docs => $2"
 echo "app/docs => $3"
 
 # Generate the markdown file
-ag "$1" @asyncapi/markdown-template -0 "./$2"
+ls -alh
+ag "$1" @asyncapi/markdown-template -o "./$2"
 echo "Generating markdown template from $1 to directory - $2"
 echo "::set-output name=status::Completed"
