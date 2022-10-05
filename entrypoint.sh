fb89141192aa3a1fd532270349e19a84c25bb016
Popsana Barida <popsyjunior@gmail.com>
2022-10-05 04:29:34 +0100

    update entrypoint file to run actual commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ echo "app/docs => $2"
 echo "app/docs => $3"
 
 # Generate the markdown file
-# ag "$1" @asyncapi/markdown-template -0 "./$2"
-# echo "Generating markdown template from $1 to directory - $2"
-# echo "::set-output name=status::Completed"
+ag "$1" @asyncapi/markdown-template -0 "./$2"
+echo "Generating markdown template from $1 to directory - $2"
+echo "::set-output name=status::Completed"
